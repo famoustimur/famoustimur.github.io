ce46df6fe52b247ac23b76e936b2e9bb44ea2c47
Timur <107718589+famoustimur@users.noreply.github.com>
2022-11-02 19:21:44 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
-$('.main__rate-star').on('mouseover', function(){
-	let starIndex = $('#main-rate-stars').children('.main__rate-star').index(this);
-	for (var i = 0; i <= $('.main__rate-star').index(this); i++) {
-		$($('#main-rate-stars').children('.main__rate-star')[i]).addClass('active');
-	}
-})
-$('.main__rate-star').on('mouseleave', function(){
-	$('.main__rate-star').removeClass('active');
-})
-$('.main__rate-star').on('click', function(){
-	$('.main__rate-star').removeClass('check');
-	let starIndex = $('#main-rate-stars').children('.main__rate-star').index(this);
-	for (var i = 0; i <= $('.main__rate-star').index(this); i++) {
-		$($('#main-rate-stars').children('.main__rate-star')[i]).addClass('check');
+var webapp = window.Telegram.WebApp, projectName = 'Durger King';
+$('#makeOrderText span').text(projectName);
+console.log(webapp);
+$('body').css('background-color', webapp.backgroundColor);
+$('header.content__header, .order__card-BTN').css('background-color', webapp.MainButton.color);
+$('.order__badge, .order__card-input').css('background-color', `${webapp.MainButton.color}40`);
+$('p.content__header-projectName, .order__card-BTN').css('color', webapp.MainButton.textColor);
+
+
+$('.order__card-BTN').on('click', function(){
+	if($(this).text() == '-'){
+		var productValue__temp = Number($(this).parent().children('.order__card-input.additBtn').val());
+		if(productValue__temp > 1)
+			$(this).parent().children('.order__card-input.additBtn').val(productValue__temp-1);
+		else{
+			$(this).parent().children().removeClass('small').addClass('hiden');
+			$(this).removeClass('hiden');
+			$(this).text('Добавить').css('background-color', webapp.MainButton.color);
+			$(this).parent().children('.order__card-input.additBtn').val(1);
+		}
+	}else if($(this).text() == '+'){
+		var productValue__temp = Number($(this).parent().children('.order__card-input.additBtn').val());
+		$(this).parent().children('.order__card-input.additBtn').val(productValue__temp+1);
+	}else{
+		$(this).parent().children().addClass('small').removeClass('hiden');
+		$(this).text('-').css('background-color', '#FA5050');
+		$(this).parent().children('.order__card-input.additBtn').val(1);
 	}
 })
